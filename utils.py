826088e284102056b63e1461bc2d4b216cfb6313
HaojieYuan <SENSETIME\yuanhaojie@g104e1900053.domain.sensetime.com>
2019-04-18 16:06:13 +0800

    load local models.

--- utils.py
+++ utils.py
@@ -1,9 +1,12 @@
 import tensorflow as tf
 import cv2
 import os
+import sys
 import numpy as np
 import facenet.src.align.detect_face as FaceDet
 from scipy import misc
+import torch
+
 
 def load_imgdir(source_imgdir, resize=None):
     """
@@ -103,3 +106,48 @@ class Detector():
         scaled = misc.imresize(cropped, (160, 160), interp='bilinear')
         
         return scaled, bbox
+
+
+
+def remove_prefix_string(string, prefix):
+    assert string.startswith(prefix), "can not remove prefix."
+    return string[len(prefix):]
+
+def remove_prefix_from_state_dict(state_dict, prefix):
+    for old_key in list(state_dict.keys()):
+        if old_key.startswith(prefix):
+            new_key = remove_prefix_string(old_key, prefix)
+            state_dict[new_key] = state_dict.pop(old_key)
+
+def load_state_local(path, model, ignore=[], optimizer=None):
+    def map_func(storage, location):
+        return storage.cuda()
+    if os.path.isfile(path):
+        print("=> loading checkpoint '{}'".format(path))
+        checkpoint = torch.load(path, map_location=map_func)
+        if len(ignore) > 0:
+            assert optimizer == None
+            for k in list(checkpoint['state_dict'].keys()):
+                flag = False
+                for prefix in ignore:
+                     if k.startswith(prefix):
+                         flag = True
+                         the_prefix = prefix
+                         break
+                if flag:
+                    print('ignoring {} (prefix: {})'.format(k, the_prefix))
+                    del checkpoint['state_dict'][k]
+        remove_prefix_from_state_dict(checkpoint['state_dict'], 'module.base.')
+        model.load_state_dict(checkpoint['state_dict'], strict=False)
+        keys1 = set(checkpoint['state_dict'].keys())
+        keys2 = set([k for k,_ in model.named_parameters()])
+        not_loaded = keys2 - keys1
+        for k in not_loaded:
+            print('caution: {} not loaded'.format(k))
+        if optimizer != None:
+            assert len(ignore) == 0
+            optimizer.load_state_dict(checkpoint['optimizer'])
+            print("=> loaded checkpoint '{}' (step {})".format(path, checkpoint['step']))
+            return checkpoint['step']
+    else:
+        assert False, "=> no checkpoint found at '{}'".format(path)
